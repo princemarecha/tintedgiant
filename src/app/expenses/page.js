03c383dd85804793399f289f501081c276cda49d
Va Marecha <pmanstylz@gmail.com>
2024-11-23 07:31:13 +0200

    Expenses Management

--- src/app/expenses/page.js
+++ src/app/expenses/page.js
@@ -3,6 +3,7 @@
 
 import Layout from "@/components/Layout";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Employee() {
   return (
@@ -12,19 +13,20 @@ export default function Employee() {
           <div className="">
           
               <div className="grid grid-cols-2 md:grid-cols-3 gap-4 mb-4">
-
-              <div className="flex flex-col items-center justify-center h-40 md:h-56 sm:h-48 font-bold rounded bg-[#AC0000] dark:bg-gray-800">
-                <Image
-                  src="/images/icons/expenses.png" // Replace with your image path
-                  alt="Dashboard Icon" // Alternative text for the image
-                  width={80} // Default width
-                  height={80} // Default height
-                  className="mb-2 transition duration-75 group-hover:opacity-80 sm:w-16 sm:h-16" // Scales image on smaller screens
-                />
-                <p className="text-xs lg:text-xl ">
-                  Manage Expenses
-                </p>
-              </div>
+              <Link href="/expenses/manage" passHref>
+                <div className="flex flex-col items-center justify-center h-40 md:h-56 sm:h-48 font-bold rounded bg-[#AC0000] dark:bg-gray-800">
+                  <Image
+                    src="/images/icons/expenses.png" // Replace with your image path
+                    alt="Dashboard Icon" // Alternative text for the image
+                    width={80} // Default width
+                    height={80} // Default height
+                    className="mb-2 transition duration-75 group-hover:opacity-80 sm:w-16 sm:h-16" // Scales image on smaller screens
+                  />
+                  <p className="text-xs lg:text-xl ">
+                    Manage Expenses
+                  </p>
+                </div>
+              </Link>
 
               <div className="flex flex-col items-center justify-center h-40 md:h-56 sm:h-48 font-bold rounded bg-[#AC0000] dark:bg-gray-800 opacity-75">
                 <Image
